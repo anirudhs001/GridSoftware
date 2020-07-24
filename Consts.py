c4b390ffe549e6e941dc64f3847c9c7e82eb3860
anirudh <anirudhs001@gmail.com>
2020-07-24 13:53:30 +0530

    Changed Sample Rate to test perceptual loss

--- Consts.py
+++ Consts.py
@@ -4,10 +4,11 @@ url_babble = "https://ecs.utdallas.edu/loizou/speech/noizeus/babble_0dB.zip"
 url_airport = "https://ecs.utdallas.edu/loizou/speech/noizeus/airport_0dB.zip"
 url_embedder = "https://drive.google.com/u/0/uc?id=1YFmhmUok-W76JkrfA0fzQt3c-ZsfiwfL&export=download"
 
-DATA_DIR = os.path.join("./datasets/processed", "size=10000")
+DATA_DIR = os.path.join("./datasets/processed", "size=10")
 MODELS_DIR = "./models/"
 
-SAMPLING_RATE = 10000
+SAMPLING_RATE = 16000
+normal_nfft = 1200
+hoplength = 160
+winlength= 400
 dvec_nfft = 512
-dvec_hoplength = 160
-dvec_winlength= 400
