7d7643fcd618105c3311466a9fa1c3d3d80d446c
anirudh <anirudhs001@gmail.com>
2020-07-26 22:56:26 +0530

    Updated Constants for Noisy Files

--- Consts.py
+++ Consts.py
@@ -1,7 +1,13 @@
+
 import os
-url_restaurant = "https://ecs.utdallas.edu/loizou/speech/noizeus/restaurant_0dB.zip"
-url_babble = "https://ecs.utdallas.edu/loizou/speech/noizeus/babble_0dB.zip"
-url_airport = "https://ecs.utdallas.edu/loizou/speech/noizeus/airport_0dB.zip"
+
+urls_Noisy = {
+    "restaurant" : "https://ecs.utdallas.edu/loizou/speech/noizeus/restaurant_0dB.zip",
+    "babble" : "https://ecs.utdallas.edu/loizou/speech/noizeus/babble_0dB.zip",
+    "airport" : "https://ecs.utdallas.edu/loizou/speech/noizeus/airport_0dB.zip",
+}
+
+url_Flipkart = "https://drive.google.com/drive/folders/1FJ6lkvJihscFfXslVJyhXIob0fX7NNd1?usp=download"
 url_embedder = "https://drive.google.com/u/0/uc?id=1YFmhmUok-W76JkrfA0fzQt3c-ZsfiwfL&export=download"
 
 DATA_DIR = os.path.join("./datasets/processed", "size=10000")
